59f753ec2dc0b67d3ee1cc21bb2cb8cbe9c2cbf6
Mind-MNK <mind.kunrattanaporn@mail.utoronto.ca>
2024-11-09 04:15:45 -0500

    Edit signup interface adapter and additional flagged from intellij

--- src/main/java/interface_adapter/signup/SignupState.java
+++ src/main/java/interface_adapter/signup/SignupState.java
@@ -8,8 +8,6 @@ public class SignupState {
     private String usernameError;
     private String password = "";
     private String passwordError;
-    private String repeatPassword = "";
-    private String repeatPasswordError;
 
     public String getUsername() {
         return username;
@@ -25,14 +23,6 @@ public class SignupState {
 
     public String getPasswordError() {
         return passwordError;
-    }
-
-    public String getRepeatPassword() {
-        return repeatPassword;
-    }
-
-    public String getRepeatPasswordError() {
-        return repeatPasswordError;
     }
 
     public void setUsername(String username) {
@@ -51,20 +41,12 @@ public class SignupState {
         this.passwordError = passwordError;
     }
 
-    public void setRepeatPassword(String repeatPassword) {
-        this.repeatPassword = repeatPassword;
-    }
-
-    public void setRepeatPasswordError(String repeatPasswordError) {
-        this.repeatPasswordError = repeatPasswordError;
-    }
 
     @Override
     public String toString() {
         return "SignupState{"
                 + "username='" + username + '\''
                 + ", password='" + password + '\''
-                + ", repeatPassword='" + repeatPassword + '\''
                 + '}';
     }
 }
