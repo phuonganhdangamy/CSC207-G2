7612b4d9d1af482fdd90a72401563eed4701b523
amvipat <amvi.patel@mail.utoronto.ca>
2024-11-11 09:57:35 -0500

    Deleted NoteView.
    Added a main method to view classes to see UI as we're building.

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -1,4 +1,20 @@
 package view;
 
+import interface_adapter.signup.SignupViewModel;
+
+import javax.swing.*;
+
 public class LoggedInView {
+
+
+
+    public static void main(String[] args){
+        //Change for this class to see UI
+        JPanel signUpView = new SignupView(new SignupViewModel());
+        JFrame frame = new JFrame();
+        frame.add(signUpView);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
