4cad3e067fe68134fcd301d41ae81da51784ef41
dorcagombar <dora.gombar@mail.utoronto.ca>
2024-11-15 17:05:55 -0500

    buy stock state and viewmodel

---
+++ src/main/java/interface_adapter/buy_stock/BuyStockState.java
@@ -0,0 +1,51 @@
+package interface_adapter.buy_stock;
+
+/**
+ * Represents the state of the buy stock process.
+ */
+
+public class BuyStockState {
+    private String selectedStock;
+    private int quantity;
+    private double userBalance;
+
+    public BuyStockState(String selectedStock, int quantity, double userBalance) {
+        this.selectedStock = selectedStock;
+        this.quantity = quantity;
+        this.userBalance = userBalance;
+    }
+
+    // Getters and Setters
+    public String getSelectedStock() {
+        return selectedStock;
+    }
+
+    public void setSelectedStock(String selectedStock) {
+        this.selectedStock = selectedStock;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUserBalance() {
+        return userBalance;
+    }
+
+    public void setUserBalance(double userBalance) {
+        this.userBalance = userBalance;
+    }
+
+    @Override
+    public String toString() {
+        return "BuyStockState{" +
+                "selectedStock='" + selectedStock + '\'' +
+                ", quantity=" + quantity +
+                ", userBalance=" + userBalance +
+                '}';
+    }
+}
---
+++ src/main/java/interface_adapter/buy_stock/BuyStockViewModel.java
@@ -0,0 +1,12 @@
+package interface_adapter.buy_stock;
+
+import interface_adapter.ViewModel;
+
+/**
+ * ViewModel for managing buy-stock-related state.
+ */
+public class BuyStockViewModel extends ViewModel<String> {
+    public BuyStockViewModel() {
+        super("buyStock");
+    }
+}
