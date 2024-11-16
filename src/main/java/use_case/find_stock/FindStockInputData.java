ac4107a7b3bc34fd1cbaf0d3797a5f91a89be9ed
Anh Dang Phuong <anh.dangphuong@mail.utoronto.ca>
2024-11-15 21:26:42 -0500

    create InMemoryUserDataAccessObject, initiate classes for find stock use case

---
+++ src/main/java/use_case/find_stock/FindStockInputData.java
@@ -0,0 +1,14 @@
+package use_case.find_stock;
+
+/**
+ * The input data for the Find Stock Use Case.
+ */
+public class FindStockInputData {
+    private String tickerSymbol;
+
+    public FindStockInputData(String tickerSymbol) {
+        this.tickerSymbol = tickerSymbol;
+    }
+
+    public String getTickerSymbol() {return tickerSymbol;}
+}
---
+++ src/main/java/use_case/find_stock/FindStockUserDataAccessInterface.java
@@ -0,0 +1,4 @@
+package use_case.find_stock;
+
+public interface FindStockUserDataAccessInterface {
+}
