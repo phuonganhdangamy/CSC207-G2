b73ff4c71789ee11a9802c049eca5f82a86bb872
Anh Dang Phuong <anh.dangphuong@mail.utoronto.ca>
2024-11-21 00:49:20 -0500

    Add Find stock State, suggest next steps for Buy stock

--- src/main/java/interface_adapter/find_stock/FindStockState.java
+++ src/main/java/interface_adapter/find_stock/FindStockState.java
@@ -1,4 +1,13 @@
 package interface_adapter.find_stock;
 
 public class FindStockState {
+    private boolean success;
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
 }
